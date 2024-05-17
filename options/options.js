be21edd5db80c3fc27652fcb282f6ff97e53682c
Crowdbotics <team@crowdbotics.com>
2024-05-17 07:45:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-moon-47999",
+  url: "https://weathered-moon-47999.botics.co",
+  api: "https://weathered-moon-47999.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
